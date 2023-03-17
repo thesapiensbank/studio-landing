d51f5145f268b6613a4594634f55eba89a0499ef
Sahil412000 <sahilpanjabi123@gmail.com>
2023-03-17 17:02:09 +0530

    Fixes

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -4,10 +4,10 @@ import logo from "../../assets/brand-logo/intro-logo.svg";
 const Introduction = () => {
   return (
     <div className="h-screen overflow-hidden w-full bg-primary flex flex-col justify-center items-center relative animate-bg opacity-0">
-      <h1 className="text-white lg:text-[13rem] md:text-[10rem] text-7xl font-intro2 absolute lg:top-72 md:top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 one uppercase">
+      <h1 className="text-white lg:text-[13rem] md:text-[10rem] text-7xl font-intro2 absolute lg:top-72 top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 one uppercase">
         Hello!
       </h1>
-      <h1 className="text-white lg:text-[13rem] md:text-[10rem] text-7xl font-intro2 absolute lg:top-72 md:top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 two w-full text-center uppercase">
+      <h1 className="text-white lg:text-[13rem] md:text-[10rem] text-7xl font-intro2 absolute lg:top-72 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 two w-full text-center uppercase">
         we are
       </h1>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 one uppercase h-max w-max md:mt-10">
