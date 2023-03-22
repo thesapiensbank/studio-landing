242b4c9ec9261403b9d28b349e5dea0c58d3a703
Sahil412000 <sahilpanjabi123@gmail.com>
2023-03-22 13:46:53 +0530

    Check

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -8,7 +8,6 @@ import { illustrations } from "../../data/portfolio/illustrations";
 import { conceptArt } from "../../data/portfolio/concept-art";
 import { visualDevelopment } from "../../data/portfolio/visual-development";
 import { fantasyArt } from "../../data/portfolio/fantasy-art";
-import { bookIllustrations } from "../../data/portfolio/book-illustrations";
 import { coverArt } from "../../data/portfolio/cover-art";
 import { childrenArt } from "../../data/portfolio/children-art";
 import { environmentalDesign } from "../../data/portfolio/environmental-design";
@@ -21,6 +20,45 @@ import {
   IconCircleChevronRight,
 } from "@tabler/icons-react";
 import Head from "next/head";
+import illustration1 from "../../public/assets/portfolio/book-illustrations/IMG_5315.jpg";
+import illustration2 from "../../public/assets/portfolio/book-illustrations/IMG_5207.jpg";
+import illustration3 from "../../public/assets/portfolio/book-illustrations/IMG_5208.jpg";
+import illustration4 from "../../public/assets/portfolio/book-illustrations/IMG_5314.jpg";
+import illustration5 from "../../public/assets/portfolio/book-illustrations/IMG_5210.jpg";
+import illustration6 from "../../public/assets/portfolio/book-illustrations/IMG_5214.jpg";
+import illustration7 from "../../public/assets/portfolio/book-illustrations/IMG_5316.jpg";
+import illustration8 from "../../public/assets/portfolio/book-illustrations/IMG_5350.jpg";
+import illustration9 from "../../public/assets/portfolio/book-illustrations/IMG_5358.jpg";
+import illustration10 from "../../public/assets/portfolio/book-illustrations/IMG_9740.jpg";
+import illustration11 from "../../public/assets/portfolio/book-illustrations/IMG_9741.jpg";
+import illustration12 from "../../public/assets/portfolio/book-illustrations/IMG_5317.jpg";
+import illustration13 from "../../public/assets/portfolio/book-illustrations/IMG_5375.jpg";
+import illustration14 from "../../public/assets/portfolio/book-illustrations/IMG_9734.jpg";
+import illustration15 from "../../public/assets/portfolio/book-illustrations/IMG_9735.jpg";
+import illustration16 from "../../public/assets/portfolio/book-illustrations/IMG_9736.jpg";
+import illustration17 from "../../public/assets/portfolio/book-illustrations/IMG_9737.jpg";
+import illustration18 from "../../public/assets/portfolio/book-illustrations/IMG_5306.jpg";
+
+const bookIllustrations = {
+  image1: illustration1,
+  image2: illustration2,
+  image3: illustration3,
+  image4: illustration4,
+  image5: illustration5,
+  image6: illustration6,
+  image7: illustration7,
+  image8: illustration8,
+  image9: illustration9,
+  image10: illustration10,
+  image11: illustration11,
+  image12: illustration12,
+  image13: illustration13,
+  image14: illustration14,
+  image15: illustration15,
+  image16: illustration16,
+  image17: illustration17,
+  image18: illustration18,
+};
 
 const Portfolio = () => {
   const ref = useRef(null);
